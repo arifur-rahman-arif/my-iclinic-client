b62d093d92e7f07dc34535e8ab515276351eb63b
Digital Technologia <dev.ar.arif@gmail.com>
2023-08-02 16:36:07 +0000

    Release/31 08 2023/2

--- src/types/pages/financeTreatment.d.ts
+++ src/types/pages/financeTreatment.d.ts
@@ -1,17 +1,21 @@
 import ImageApiType from 'src/types/api/image';
 
 export default interface FinanceTreatmentPageContents {
-    section_1: {
-        heading: string;
-        descriptions: string[];
-        image: ImageApiType;
-        large_image: ImageApiType;
-    };
-    calculator_heading: string;
-    // Our health insurance partners
-    section_3: string;
-    // Insurance partners logo
-    section_4: string;
-    // Healthcare insurance provider
-    section_5: string;
+	section_1: {
+		heading: string;
+		descriptions: string[];
+		image: ImageApiType;
+		large_image: ImageApiType;
+	};
+	section_2: {
+		heading: string;
+		subheading: string;
+		description: string;
+	};
+	// Our health insurance partners
+	section_3: string;
+	// Insurance partners logo
+	section_4: string;
+	// Healthcare insurance provider
+	section_5: string;
 }
