b21b29a1bf501e7267a2b7e4933e1f6a16d62cc0
DigitalTechnologia <dev.ar.arif@gmail.com>
2023-10-09 17:24:33 +0600

    Added pabao link

--- src/components/Header/ConsultationButtons.tsx
+++ src/components/Header/ConsultationButtons.tsx
@@ -31,7 +31,7 @@ const ConsultationButtons = ({
                 modalElement={
                     <>
                         <iframe
-                            src="https://calendly.com/myiclinic/free_visioncorrection_consultation"
+                            src="https://connect.pabau.com/bookings.php?compid=11842"
                             width={600}
                             height={700}
                             className="w-full md:min-h-[70rem]"
