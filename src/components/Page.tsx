813197a0dc48defae9b5d3257648c4a6ea7b5096
Websider <dev.ar.arif@gmail.com>
2024-05-08 07:02:11 +0000

    Moved the scripts back

--- src/components/Page.tsx
+++ src/components/Page.tsx
@@ -1,7 +1,6 @@
 // @ts-nocheck
 import HTMLReactParser from 'html-react-parser';
 import Head from 'next/head';
-// import Script from 'next/script';
 import { useRouter } from 'next/router';
 
 interface PropInterface {
@@ -30,6 +29,57 @@ const Page = ({ children, title, description, seo, yoastJson }: PropInterface): 
     return (
         <>
             <Head>
+                {/* Google Tag Manager */}
+                <script
+                    async
+                    dangerouslySetInnerHTML={{
+                        __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+                                new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+                                j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+                                'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+                                })(window,document,'script','dataLayer','GTM-MFT6DQQ');`
+                    }}
+                />
+                {/* End Google Tag Manager */}
+                <script async={true} src="https://www.googletagmanager.com/gtag/js?id=AW-970733853"></script>
+                <script
+                    async
+                    dangerouslySetInnerHTML={{
+                        __html: `window.dataLayer = window.dataLayer || []; function gtag(){dataLayer.push(arguments);} gtag('js', new Date()); gtag('config', 'AW-970733853');`
+                    }}
+                />
+
+                {/* Meta Pixel Code */}
+                <script async
+                        dangerouslySetInnerHTML={{
+                            __html: `!function(f,b,e,v,n,t,s)
+                    
+                    {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+                        
+                        n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+                        
+                        if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+                        
+                        n.queue=[];t=b.createElement(e);t.async=!0;
+                        
+                        t.src=v;s=b.getElementsByTagName(e)[0];
+                        
+                        s.parentNode.insertBefore(t,s)}(window, document,'script',
+                    
+                    'https://connect.facebook.net/en_US/fbevents.js');
+                    
+                    fbq('init', '3435821386686735');
+                    
+                    fbq('track', 'PageView');`
+                        }}
+                />
+                <noscript><img alt="" height="1" width="1" style={{
+                    display: 'none'
+                }} src="https://www.facebook.com/tr?id=3435821386686735&ev=PageView&noscript=1"
+
+                /></noscript>
+                {/* End Meta Pixel Code */}
+
                 {!yoastJson?.title && <title>{title}</title>}
                 {!yoastJson?.description && <meta name="description" content={description} />}
 
@@ -40,7 +90,22 @@ const Page = ({ children, title, description, seo, yoastJson }: PropInterface): 
                 {seo && HTMLReactParser(seo)}
             </Head>
 
-            {children}
+            {/* Google Tag Manager (noscript) */}
+            <noscript
+                dangerouslySetInnerHTML={{
+                    __html: `
+                            <iframe
+                              src="https://www.googletagmanager.com/ns.html?id=GTM-MFT6DQQ"
+                              height="0"
+                              width="0"
+                              style={{ display: 'none', visibility: 'hidden' }}
+                            ></iframe>
+                          `
+                }}
+            />
+                {/* End Google Tag Manager (noscript) */}
+
+                {children}
         </>
     );
 };
