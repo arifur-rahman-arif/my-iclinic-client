348c067a7503fb3ab79cdfe2c82f3c0e2b44ef09
AR. Arif <dev.ar.arif@gmail.com>
2024-06-18 21:55:20 +0600

    Completed page indexing

---
+++ algolia/articles.js
@@ -0,0 +1,138 @@
+require('dotenv').config();
+const algoliasearch = require('algoliasearch');
+const striptags = require('striptags');
+const { sliceStringByWordsAndSizeLimit } = require('./pages');
+
+if (!process.env?.NEXT_PUBLIC_ALGOLIA) return;
+// My-iClinic-dev
+const indexName = process.env.NEXT_PUBLIC_ALGOLIA_DB || 'My-iClinic';
+
+const client = algoliasearch('LFKQJW9O2S', '47d64d3c035e3b58c0efafcc0d89e6ed');
+const index = client.initIndex(indexName);
+index.clearObjects();
+
+/**
+ * ==================================================================================
+ * ==================================================================================
+ * ==================================================================================
+ * ==================================================================================
+ * ==================================================================================
+ * This block of code is responsible for saving the content of our pages into Algolia.
+ * It specifically targets articles and specialists.
+ * ==================================================================================
+ * ==================================================================================
+ * ==================================================================================
+ * ==================================================================================
+ * ==================================================================================
+ */
+
+/**
+ * Get the post in formatted way
+ * @returns {Promise<*>}
+ */
+const getPosts = async () => {
+    const totalPosts = await getPostsCount();
+    const postsPerPage = 100;
+    const totalPages = Math.ceil(totalPosts / postsPerPage);
+    const data = [];
+
+    for (let page = 1; page <= totalPages; page++) {
+        const apiResponse = await fetch(
+            `${process.env.WP_REST_URL}/posts?_fields=id,title,content,slug&per_page=${postsPerPage}&page=${page}`,
+            {
+                headers: {
+                    'Content-Type': 'application/json; charset=UTF-8',
+                    Authorization: `Bearer ${process.env.WP_JWT_TOKEN}`
+                }
+            }
+        );
+
+        if (apiResponse.status !== 200) {
+            throw new Error('Unable to fetch WordPress posts. Error text: ' + apiResponse.statusText);
+        }
+
+        const responseData = await apiResponse.json();
+        data.push(
+            ...responseData.map((post) => ({
+                objectID: post.id,
+                title: post?.title?.rendered || '',
+                content: sliceStringByWordsAndSizeLimit(striptags(post?.content?.rendered, [], ' ')),
+                section: `/articles/${post.slug}`,
+                type: 'article'
+            }))
+        );
+    }
+
+    return data;
+};
+
+/**
+ * Get total posts count from WordPress
+ * @returns {Promise<number>}
+ */
+const getPostsCount = async () => {
+    const apiResponse = await fetch(`${process.env.WP_REST_URL}/posts?_fields=id&per_page=1`, {
+        headers: {
+            'Content-Type': 'application/json; charset=UTF-8',
+            Authorization: `Bearer ${process.env.WP_JWT_TOKEN}`
+        }
+    });
+
+    if (apiResponse.status !== 200) {
+        throw new Error('Unable to fetch WordPress posts count. Error text: ' + apiResponse.statusText);
+    }
+
+    const { headers } = apiResponse;
+    const headerObject = Object.fromEntries(headers.entries());
+
+    return Number(headerObject['x-wp-total']);
+};
+
+getPosts().then((data) => {
+    index
+        .saveObjects(data)
+        .then((res) => {
+            console.log('Articles saved:', res?.objectIDs.length);
+            console.log(res);
+        })
+        .catch((err) => {
+            console.log(err);
+        });
+});
+
+const getSpecialistsPost = async () => {
+    const apiResponse = await fetch(
+        `${process.env.WP_REST_URL}/specialist?_fields=id,title,content,slug,acf&per_page=50`,
+        {
+            headers: {
+                'Content-Type': 'application/json; charset=UTF-8',
+                Authorization: `Bearer ${process.env.WP_JWT_TOKEN}`
+            }
+        }
+    );
+
+    const posts = await apiResponse.json();
+
+    return posts.map((post) => {
+        return {
+            objectID: post.id,
+            title: post?.title?.rendered || '',
+            designation: post.acf.specialist_data.degree || '' + ' ' + post.acf.specialist_data.title || '',
+            content: sliceStringByWordsAndSizeLimit(striptags(post?.content?.rendered, [], ' ')),
+            section: `/our-specialists/${post.slug}`,
+            type: 'specialist'
+        };
+    });
+};
+
+getSpecialistsPost().then((data) => {
+    index
+        .saveObjects(data)
+        .then((res) => {
+            console.log('Specialist saved', res?.objectIDs.length);
+            console.log(res);
+        })
+        .catch((err) => {
+            console.log(err);
+        });
+});
--- algolia/pages.js
+++ algolia/pages.js
@@ -5,7 +5,8 @@ const axios = require('axios');
 const cheerio = require('cheerio');
 
 if (!process.env?.NEXT_PUBLIC_ALGOLIA) return;
-const indexName = process.env.NEXT_PUBLIC_ALGOLIA ? 'My-iClinic' : 'My-iClinic-dev';
+// My-iClinic-dev
+const indexName = process.env.NEXT_PUBLIC_ALGOLIA_DB || 'My-iClinic';
 
 const client = algoliasearch('LFKQJW9O2S', '47d64d3c035e3b58c0efafcc0d89e6ed');
 const index = client.initIndex(indexName);
@@ -36,6 +37,10 @@ const sliceStringByWordsAndSizeLimit = (string) => {
     }
 
     return result;
+};
+
+module.exports = {
+    sliceStringByWordsAndSizeLimit
 };
 
 /**
@@ -138,7 +143,6 @@ const extractSectionsFromHTML = (html) => {
 
         if (title) {
             sections.push({
-                objectID: i,
                 title,
                 content: sliceStringByWordsAndSizeLimit(striptags(contentArray.toString(), [], ' ').trim()), // Example word limit
                 section: id ? `#${id}` : ''
@@ -150,21 +154,27 @@ const extractSectionsFromHTML = (html) => {
 };
 
 const indexDataToAlgolia = async (data) => {
-    try {
-        index.clearObjects();
+    const chunkSize = 50;
+    const delay = 1000;
+    let totalRecord = 0;
 
-        index
-            .saveObjects(data, { autoGenerateObjectIDIfNotExist: true })
-            .then((res) => {
-                console.log('Page content saved');
-                console.log(res);
-            })
-            .catch((err) => {
-                console.log(err);
-            });
-    } catch (error) {
-        console.error('Error indexing data to Algolia:', error);
+    for (let i = 0; i < data.length; i += chunkSize) {
+        const chunk = data.slice(i, i + chunkSize);
+
+        try {
+            const response = await index.saveObjects(chunk, { autoGenerateObjectIDIfNotExist: true });
+
+            totalRecord += response?.objectIDs?.length;
+
+            console.log(`Page content saved: ${i + 1} to ${i + chunkSize}`);
+
+            await new Promise((resolve) => setTimeout(resolve, delay));
+        } catch (error) {
+            console.error('Error indexing data to Algolia:', error);
+        }
     }
+
+    console.log(`Total records saved: ${totalRecord}`);
 };
 
 const processPages = async (pages) => {
@@ -187,130 +197,4 @@ const processPages = async (pages) => {
     await indexDataToAlgolia(sectionsData);
 };
 
-await processPages(pages);
-
-/**
- * ==================================================================================
- * ==================================================================================
- * ==================================================================================
- * ==================================================================================
- * ==================================================================================
- * This block of code is responsible for saving the content of our pages into Algolia.
- * It specifically targets articles and specialists.
- * ==================================================================================
- * ==================================================================================
- * ==================================================================================
- * ==================================================================================
- * ==================================================================================
- */
-
-/**
- * Get the post in formatted way
- * @returns {Promise<*>}
- */
-const getPosts = async () => {
-    const totalPosts = await getPostsCount();
-    const postsPerPage = 100;
-    const totalPages = Math.ceil(totalPosts / postsPerPage);
-    const data = [];
-
-    for (let page = 1; page <= totalPages; page++) {
-        const apiResponse = await fetch(
-            `${process.env.WP_REST_URL}/posts?_fields=id,title,content,slug&per_page=${postsPerPage}&page=${page}`,
-            {
-                headers: {
-                    'Content-Type': 'application/json; charset=UTF-8',
-                    Authorization: `Bearer ${process.env.WP_JWT_TOKEN}`
-                }
-            }
-        );
-
-        if (apiResponse.status !== 200) {
-            throw new Error('Unable to fetch WordPress posts. Error text: ' + apiResponse.statusText);
-        }
-
-        const responseData = await apiResponse.json();
-        data.push(
-            ...responseData.map((post) => ({
-                objectID: post.id,
-                title: post?.title?.rendered || '',
-                content: sliceStringByWordsAndSizeLimit(striptags(post?.content?.rendered, [], ' ')),
-                section: `/articles/${post.slug}`,
-                type: 'article'
-            }))
-        );
-    }
-
-    return data;
-};
-
-/**
- * Get total posts count from WordPress
- * @returns {Promise<number>}
- */
-const getPostsCount = async () => {
-    const apiResponse = await fetch(`${process.env.WP_REST_URL}/posts?_fields=id&per_page=1`, {
-        headers: {
-            'Content-Type': 'application/json; charset=UTF-8',
-            Authorization: `Bearer ${process.env.WP_JWT_TOKEN}`
-        }
-    });
-
-    if (apiResponse.status !== 200) {
-        throw new Error('Unable to fetch WordPress posts count. Error text: ' + apiResponse.statusText);
-    }
-
-    const { headers } = apiResponse;
-    const headerObject = Object.fromEntries(headers.entries());
-
-    return Number(headerObject['x-wp-total']);
-};
-
-getPosts().then((data) => {
-    index
-        .saveObjects(data)
-        .then((res) => {
-            console.log('Articles saved');
-            console.log(res);
-        })
-        .catch((err) => {
-            console.log(err);
-        });
-});
-
-const getSpecialistsPost = async () => {
-    const apiResponse = await fetch(
-        `${process.env.WP_REST_URL}/specialist?_fields=id,title,content,slug,acf&per_page=50`,
-        {
-            headers: {
-                'Content-Type': 'application/json; charset=UTF-8',
-                Authorization: `Bearer ${process.env.WP_JWT_TOKEN}`
-            }
-        }
-    );
-
-    const posts = await apiResponse.json();
-
-    return posts.map((post) => {
-        return {
-            objectID: post.id,
-            title: post?.title?.rendered || '',
-            designation: post.acf.specialist_data.degree || '' + ' ' + post.acf.specialist_data.title || '',
-            content: sliceStringByWordsAndSizeLimit(striptags(post?.content?.rendered, [], ' ')),
-            section: `/our-specialists/${post.slug}`,
-            type: 'specialist'
-        };
-    });
-};
-
-getSpecialistsPost().then((data) => {
-    index
-        .saveObjects(data)
-        .then((res) => {
-            console.log('Specialist saved');
-            console.log(res);
-        })
-        .catch((err) => {
-            console.log(err);
-        });
-});
+processPages(pages);
